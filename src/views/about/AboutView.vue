c2ca657a0f9dd8b18d2dcd34c5780cdb3177d24b
Alneys <35741726+Alneys@users.noreply.github.com>
2024-06-14 17:59:05 +0800

    fix(build): type check on el-link

--- src/views/about/AboutView.vue
+++ src/views/about/AboutView.vue
@@ -13,10 +13,10 @@ function increment() {
     <div class="al-divider"></div>
     <h2>Alneys</h2>
     <p>
-      <el-link type="" href="https://github.com/Alneys" target="_blank"> GitHub @Alneys </el-link>
+      <el-link type="default" href="https://github.com/Alneys" target="_blank"> GitHub @Alneys </el-link>
     </p>
     <p>
-      <el-link type="" href="https://twitter.com/Alneys_Al" target="_blank">
+      <el-link type="default" href="https://twitter.com/Alneys_Al" target="_blank">
         X / Twitter @Alneys_Al
       </el-link>
     </p>
