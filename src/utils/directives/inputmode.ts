bad1bc2eb98acf1213c542487a8aaefcc3fa7341
Alneys <35741726+Alneys@users.noreply.github.com>
2024-07-04 10:54:08 +0800

    feat: v-inputmode directive

---
+++ src/utils/directives/inputmode.ts
@@ -0,0 +1,21 @@
+import type { Directive } from 'vue';
+
+const inputmodeDirective: Directive = {
+  mounted(el: Element, binding, vnode, prevVnode) {
+    try {
+      if (el.tagName.toLowerCase() !== 'input') {
+        el = el.getElementsByTagName('input')[0];
+        if (!el) {
+          throw 'Input element not found';
+        }
+      }
+      if (binding.value) {
+        el.setAttribute('inputmode', binding.value);
+      }
+    } catch (error) {
+      console.error('v-inputmode error: ', error);
+    }
+  },
+};
+
+export default inputmodeDirective;
