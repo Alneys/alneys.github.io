d39c2cc9e8612ce1c8af6500dc747fef64d88470
Alneys <35741726+Alneys@users.noreply.github.com>
2026-01-06 11:20:17 +0800

    feat(cgss): release of unit viewer (resonance only)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,8 +11,8 @@ const env = import.meta.env;
     </div>
 
     <div class="main">
-      <router-link v-if="env.DEV" to="/cgss/unit-viewer">
-        <el-card shadow="hover">（开发中）偶像大师灰姑娘女孩星光舞台 组队参考</el-card>
+      <router-link to="/cgss/unit-viewer">
+        <el-card shadow="hover">偶像大师灰姑娘女孩星光舞台 组队信息</el-card>
       </router-link>
       <router-link to="/mltd/anniversary-calc">
         <el-card shadow="hover">偶像大师百万现场 周年活动计算器</el-card>
