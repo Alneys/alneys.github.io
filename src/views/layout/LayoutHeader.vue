88a6d55bf712809d2a6bc90efd865f54d8f5c8a7
Alneys <35741726+Alneys@users.noreply.github.com>
2026-04-26 02:20:10 +0800

    feat: dark mode experimental

--- src/views/layout/LayoutHeader.vue
+++ src/views/layout/LayoutHeader.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { useDark } from '@vueuse/core';
+import { Sunny, Moon } from '@element-plus/icons-vue';
 import CgssCountdownHeader from '@/components/CgssCountdownHeader.vue';
+
+const isDark = useDark();
 </script>
 
 <template>
   <header id="layout-header">
     <div class="layout-max-width-container">
       <router-link to="/" class="title"><h1>Alneys.github.io</h1></router-link>
+      <el-switch v-model="isDark" :inactive-action-icon="Sunny" :active-action-icon="Moon" />
     </div>
     <!-- <CgssCountdownHeader></CgssCountdownHeader> -->
   </header>
