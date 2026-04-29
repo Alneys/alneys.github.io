36f11a96a1685581d5f3d4a04f55716c2e89c6cd
Alneys <35741726+Alneys@users.noreply.github.com>
2026-04-29 11:13:46 +0800

    fix(mltd): input parser

--- src/views/mltd/MltdEventParking.vue
+++ src/views/mltd/MltdEventParking.vue
@@ -1,3 +1,141 @@
+<template>
+  <div id="view-mltd-event-parking">
+    <h1 class="view-title">偶像大师百万现场 活动控分计算器</h1>
+    <div class="al-divider"></div>
+    <div style="margin-bottom: 1em">
+      <el-alert type="error" :closable="false" show-icon>
+        <h2>警告：本页面正在开发中，无法保证结果的准确性！</h2>
+      </el-alert>
+    </div>
+    <div id="mltd-event-parking-form">
+      <el-row :gutter="16">
+        <el-col :lg="13" :sm="24">
+          <el-form
+            ref="formRef"
+            :model="form"
+            label-width="auto"
+            label-position="top"
+            style="max-width: 800px"
+          >
+            <el-form-item label="选择活动类型">
+              <el-select v-model="form.eventType">
+                <el-option label="Theater / Trust" :value="3"></el-option>
+                <el-option label="Anniversary" :value="5"></el-option>
+                <el-option label="[开发中] Tour / Tour Bingo" :value="4" disabled></el-option>
+                <el-option label="其他活动开发中" :value="0" disabled></el-option>
+                <!-- 1: Showtime -->
+                <!-- 2: Millicolle! -->
+                <!-- 3: Theater / Trust -->
+                <!-- 4: Tour / Tour Bingo -->
+                <!-- 5: Anniversary -->
+                <!-- 6: Working -->
+                <!-- 7: April Fool -->
+                <!-- 8: Game Corner -->
+                <!-- 9: Millicolle! (Box Gasha) -->
+                <!-- 10: Twin Stage (High Score by Song) -->
+                <!-- 11: Tune -->
+                <!-- 12: Twin Stage (Total High Score) -->
+                <!-- 13: Tale / Time / Team -->
+                <!-- 14: Talk Party -->
+                <!-- 16: Treasure -->
+              </el-select>
+            </el-form-item>
+            <el-row :gutter="16" @keyup.enter="handleSubmit">
+              <el-col :span="8" :xs="24">
+                <el-form-item label="目标pt" prop="targetPt">
+                  <template #label><b>目标pt</b></template>
+                  <el-input
+                    v-model.number="form.targetPt"
+                    :min="0"
+                    :max="99999999"
+                    type="number"
+                    maxlength="8"
+                    inputmode="numeric"
+                    placeholder="0"
+                  >
+                    <template #append>pt</template>
+                  </el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8" :xs="24">
+                <el-form-item label="当前pt" prop="pt">
+                  <el-input
+                    v-model.number="form.pt"
+                    :min="0"
+                    :max="99999999"
+                    type="number"
+                    maxlength="8"
+                    inputmode="numeric"
+                    placeholder="0"
+                  >
+                    <template #append>pt</template>
+                  </el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8" :xs="24">
+                <el-form-item label="道具数" prop="token">
+                  <el-input
+                    v-model.number="form.token"
+                    :min="0"
+                    :max="999999"
+                    type="number"
+                    maxlength="6"
+                    inputmode="numeric"
+                    placeholder="0"
+                  >
+                    <template #append>个</template>
+                  </el-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <el-form-item label=" ">
+              <el-space wrap>
+                <el-button type="primary" @click="handleSubmit">开始计算</el-button>
+                <el-button @click="handleClear">清空</el-button>
+              </el-space>
+            </el-form-item>
+          </el-form>
+          <el-alert v-show="form.eventType === 5" type="warning" :closable="false" show-icon>
+            <p style="font-size: var(--el-font-size-base)">
+              注意：周年活动有每日推荐曲和普通曲的区别
+            </p>
+          </el-alert>
+        </el-col>
+        <el-col :span="0.1" class="hidden-sm-and-down">
+          <div class="al-divider-vertical" style="margin: 0 0.5%"></div>
+        </el-col>
+        <el-col :lg="0" :sm="24">
+          <div class="al-divider"></div>
+        </el-col>
+        <el-col :lg="10" :sm="24">
+          <div id="mltd-event-parking-result" style="margin-bottom: 2em">
+            <h2>结果</h2>
+            <div v-if="calculatedFlag">
+              <p v-if="parkingResult?.flag === false">控分失败：{{ parkingResult.message }}</p>
+              <div v-else>
+                <h4>当前状态</h4>
+                <p>pt差距：{{ (form.targetPt! - form.pt!).toLocaleString('en-US') }}</p>
+                <p>道具数：{{ form.token!.toLocaleString('en-US') }}</p>
+
+                <h4>控分方案</h4>
+                <template v-for="each of parkingResult?.result" :key="each.name">
+                  <p v-if="each.value > 0">
+                    {{ each.name }} {{ each.multiplier }} ：{{ each.value }}次
+                  </p>
+                </template>
+              </div>
+            </div>
+            <div v-else>
+              <p>等待输入</p>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <!-- <div class="al-divider"></div> -->
+  </div>
+</template>
+
 <script setup lang="ts">
 import { ref, reactive, nextTick } from 'vue';
 import type { FormInstance } from 'element-plus';
@@ -157,144 +295,6 @@ function calcParkingTheater(
   }
 }
 </script>
-
-<template>
-  <div id="view-mltd-event-parking">
-    <h1 class="view-title">偶像大师百万现场 活动控分计算器</h1>
-    <div class="al-divider"></div>
-    <div style="margin-bottom: 1em">
-      <el-alert type="error" :closable="false" show-icon>
-        <h2>警告：本页面正在开发中，无法保证结果的准确性！</h2>
-      </el-alert>
-    </div>
-    <div id="mltd-event-parking-form">
-      <el-row :gutter="16">
-        <el-col :lg="13" :sm="24">
-          <el-form
-            ref="formRef"
-            :model="form"
-            label-width="auto"
-            label-position="top"
-            style="max-width: 800px"
-          >
-            <el-form-item label="选择活动类型">
-              <el-select v-model="form.eventType">
-                <el-option label="Theater / Trust" :value="3"></el-option>
-                <el-option label="Anniversary" :value="5"></el-option>
-                <el-option label="[开发中] Tour / Tour Bingo" :value="4" disabled></el-option>
-                <el-option label="其他活动开发中" :value="0" disabled></el-option>
-                <!-- 1: Showtime -->
-                <!-- 2: Millicolle! -->
-                <!-- 3: Theater / Trust -->
-                <!-- 4: Tour / Tour Bingo -->
-                <!-- 5: Anniversary -->
-                <!-- 6: Working -->
-                <!-- 7: April Fool -->
-                <!-- 8: Game Corner -->
-                <!-- 9: Millicolle! (Box Gasha) -->
-                <!-- 10: Twin Stage (High Score by Song) -->
-                <!-- 11: Tune -->
-                <!-- 12: Twin Stage (Total High Score) -->
-                <!-- 13: Tale / Time / Team -->
-                <!-- 14: Talk Party -->
-                <!-- 16: Treasure -->
-              </el-select>
-            </el-form-item>
-            <el-row :gutter="16" @keyup.enter="handleSubmit">
-              <el-col :span="8" :xs="24">
-                <el-form-item label="目标pt" prop="targetPt">
-                  <template #label><b>目标pt</b></template>
-                  <el-input
-                    v-model.number="form.targetPt"
-                    :min="0"
-                    :max="99999999"
-                    :formatter="(value: string) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
-                    :parser="(value: string) => value.replace(/\$\s?|(,*)/g, '')"
-                    inputmode="numeric"
-                    placeholder="100,000"
-                  >
-                    <template #append>pt</template>
-                  </el-input>
-                </el-form-item>
-              </el-col>
-              <el-col :span="8" :xs="24">
-                <el-form-item label="当前pt" prop="pt">
-                  <el-input
-                    v-model.number="form.pt"
-                    :min="0"
-                    :max="99999999"
-                    :formatter="(value: string) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
-                    :parser="(value: string) => value.replace(/\$\s?|(,*)/g, '')"
-                    inputmode="numeric"
-                    placeholder="99,039"
-                  >
-                    <template #append>pt</template>
-                  </el-input>
-                </el-form-item>
-              </el-col>
-              <el-col :span="8" :xs="24">
-                <el-form-item label="道具数" prop="token">
-                  <el-input
-                    v-model.number="form.token"
-                    :min="0"
-                    :max="999999"
-                    :formatter="(value: string) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
-                    :parser="(value: string) => value.replace(/\$\s?|(,*)/g, '')"
-                    inputmode="numeric"
-                    placeholder="1,300"
-                  >
-                    <template #append>个</template>
-                  </el-input>
-                </el-form-item>
-              </el-col>
-            </el-row>
-            <el-form-item label=" ">
-              <el-space wrap>
-                <el-button type="primary" @click="handleSubmit">开始计算</el-button>
-                <el-button @click="handleClear">清空</el-button>
-              </el-space>
-            </el-form-item>
-          </el-form>
-          <el-alert v-show="form.eventType === 5" type="warning" :closable="false" show-icon>
-            <p style="font-size: var(--el-font-size-base)">
-              注意：周年活动有每日推荐曲和普通曲的区别
-            </p>
-          </el-alert>
-        </el-col>
-        <el-col :span="0.1" class="hidden-sm-and-down">
-          <div class="al-divider-vertical" style="margin: 0 0.5%"></div>
-        </el-col>
-        <el-col :lg="0" :sm="24">
-          <div class="al-divider"></div>
-        </el-col>
-        <el-col :lg="10" :sm="24">
-          <div id="mltd-event-parking-result" style="margin-bottom: 2em">
-            <h2>结果</h2>
-            <div v-if="calculatedFlag">
-              <p v-if="parkingResult?.flag === false">控分失败：{{ parkingResult.message }}</p>
-              <div v-else>
-                <h4>当前状态</h4>
-                <p>pt差距：{{ (form.targetPt! - form.pt!).toLocaleString('en-US') }}</p>
-                <p>道具数：{{ form.token!.toLocaleString('en-US') }}</p>
-
-                <h4>控分方案</h4>
-                <template v-for="each of parkingResult?.result" :key="each.name">
-                  <p v-if="each.value > 0">
-                    {{ each.name }} {{ each.multiplier }} ：{{ each.value }}次
-                  </p>
-                </template>
-              </div>
-            </div>
-            <div v-else>
-              <p>等待输入</p>
-            </div>
-          </div>
-        </el-col>
-      </el-row>
-    </div>
-    <!-- <div class="al-divider"></div> -->
-  </div>
-</template>
 
 <style lang="scss" scoped>
 :deep() {
