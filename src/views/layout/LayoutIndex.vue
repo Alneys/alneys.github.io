debb7296c259aca030e4ab8aa1ec91cf329c59a0
Alneys <35741726+Alneys@users.noreply.github.com>
2026-05-09 15:13:11 +0800

    feat: add back button on about page

--- src/views/layout/LayoutIndex.vue
+++ src/views/layout/LayoutIndex.vue
@@ -22,8 +22,22 @@ useVersionCheck();
   max-width: 1664px;
 }
 
-#layout > * h1.view-title {
+#layout h1.view-title {
   margin-top: 0.25em;
+  margin-bottom: 0.25em;
+  line-height: 1;
+}
+
+#layout .view-page-header {
+  margin-top: 0.25em;
+  margin-bottom: 0.25em;
+  font-size: 24px;
+  line-height: 1;
+
+  .el-page-header__content h1 {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
 }
 
 @media screen and (max-width: 767px) {
