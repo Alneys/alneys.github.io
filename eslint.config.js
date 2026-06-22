ca59be5df705bdd15111126c4356917afcf096f8
Alneys <35741726+Alneys@users.noreply.github.com>
2026-06-22 14:49:57 +0800

    build: eslint 10, typescript 6

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,45 @@
+import { globalIgnores } from 'eslint/config';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 import pluginVue from 'eslint-plugin-vue';
-import vueTsEslintConfig from '@vue/eslint-config-typescript';
-import eslintConfigPrettier from '@vue/eslint-config-prettier';
 import pluginVitest from '@vitest/eslint-plugin';
+import skipFormatting from 'eslint-config-prettier/flat';
 
-/** @type {import('eslint/config').Config[]} */
-export default [
-  {
-    ignores: [
-      '.local/**',
-      'node_modules/**',
-      'dist/**',
-      'dist-ssr/**',
-      'coverage/**',
-      '*.tsbuildinfo',
-      'components.d.ts',
-      'auto-imports.d.ts',
-    ],
-  },
+// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
+// import { configureVueProject } from '@vue/eslint-config-typescript'
+// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    files: ['**/*.{vue,ts,mts,tsx}'],
   },
 
+  globalIgnores([
+    'dist/**',
+    'dist-ssr/**',
+    'coverage/**',
+    '.local/**',
+    '*.tsbuildinfo',
+    'components.d.ts',
+    'auto-imports.d.ts',
+  ]),
+
   ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
+  vueTsConfigs.recommended,
 
   {
     ...pluginVitest.configs.recommended,
-    files: ['**/*.{spec,test}.ts'],
+    files: ['src/**/__tests__/*'],
   },
 
-  eslintConfigPrettier,
+  skipFormatting,
 
   {
-    languageOptions: {
-      ecmaVersion: 'latest',
-    },
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
       'vue/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
-];
+);
